b3043fdbc641b5e9c6f00e5127a088c860831609
William Sawyerr <152267523+wsdbsuid798@users.noreply.github.com>
2024-02-11 22:30:04 +0000

    Chore: :zap: Update codebase

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-700 text-white p-4 text-center">
+        <p>&copy; 2024 DBS. All rights reserved.</p>
+      </footer>
+        )
+  }
---
+++ components/Navbar.js
@@ -0,0 +1,7 @@
+export default function Navbar() {
+    return (
+        <nav className="bg-gray-800 text-white p-4">
+        <h1 className="text-xl">GDP MAN DBS</h1>
+      </nav>
+        )
+  }
